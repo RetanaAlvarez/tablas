f9101a6d44ac74c72186eaf6d1523be5491fd6ee
AaronCG04 <aaron.cruzgonzalez@gmail.com>
2020-11-24 17:59:51 -0600

    creacion de tablas unidades y alumnos

--- tablas.sql
+++ tablas.sql
@@ -1,5 +1,18 @@
-create table alumnos(
-num_control varchar(13) primary key,
+// tabla de alumnos 
+CREATE TABLE alumnos(
+num_control varchar(13) NOT NULL PRIMARY KEY,
 id_persona int,
-contraint per_alu foreing key(id_persona) references personas(id_persona)
+constraint per_alu FOREIGN KEY (id_persona) REFERENCES persona(id_persona)
+ON DELETE CASCADE
 );
+//tabla unidades
+CREATE TABLE unidades(
+id_unidad varchar(10) NOT NULL PRIMARY KEY,
+num_unidad number(1),
+descripcion varchar(30),
+clave_materia int,
+CONSTRAINT materia_unidades FOREIGN KEY(clave_materia) REFERENCES materias(clave_materia)
+ON DELETE CASCADE
+);
+// agregar sus incersiones de codigo de la tabla alumno
+// agregar sus increciones de codigo de la tabla unidades
