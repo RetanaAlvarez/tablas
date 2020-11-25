4379d64605aa2d44566d882b6c394b5e5b39933c
AaronCG04 <aaron.cruzgonzalez@gmail.com>
2020-11-24 19:20:41 -0600

    Creacion de 10 regitros para cada tabla --Aaron

--- tablas.sql
+++ tablas.sql
@@ -9,10 +9,30 @@ ON DELETE CASCADE
 CREATE TABLE unidades(
 id_unidad varchar(10) NOT NULL PRIMARY KEY,
 num_unidad number(1),
-descripcion varchar(30),
+descripcion varchar(50),
 clave_materia int,
 CONSTRAINT materia_unidades FOREIGN KEY(clave_materia) REFERENCES materias(clave_materia)
 ON DELETE CASCADE
 );
 --agregar sus incersiones de codigo de la tabla alumno
+INSERT INTO alumnos VALUES('2018150480195',pendiente);
+INSERT INTO alumnos VALUES('2018150480034',pendiente);
+INSERT INTO alumnos VALUES('2018150480572',pendiente);
+INSERT INTO alumnos VALUES('2018150480246',pendiente);
+INSERT INTO alumnos VALUES('2018150480125',pendiente);
+INSERT INTO alumnos VALUES('2018150480873',pendiente);
+INSERT INTO alumnos VALUES('2018150480164',pendiente);
+INSERT INTO alumnos VALUES('2018150480073',pendiente);
+INSERT INTO alumnos VALUES('2018150480724',pendiente);
+INSERT INTO alumnos VALUES('2018150480936',pendiente);
 --agregar sus increciones de codigo de la tabla unidades
+INSERT INTO unidades VALUES('clavemateria-1',1,'Fundamentos de Ingeneria en Software',pendiente);
+INSERT INTO unidades VALUES('clavemateria-5',5,'Introduccion a la Animacion Por Computadora',pendiente);
+INSERT INTO unidades VALUES('clavemateria-2',2,'El modelo de negocio',pendiente);
+INSERT INTO unidades VALUES('clavemateria-3',3,'Ingenria de Requerimentos',pendiente);
+INSERT INTO unidades VALUES('clavemateria-4',4,'Modelo de Analisis',pendiente);
+INSERT INTO unidades VALUES('clavemateria-5',5,'Calidad de Software',pendiente);
+INSERT INTO unidades VALUES('clavemateria-1',1,'Introduccion a la Graficacion Por Computadora',pendiente);
+INSERT INTO unidades VALUES('clavemateria-2',2,'Graficacion 2D',pendeinte);
+INSERT INTO unidades VALUES('clavemateria-3',3,'Graficacion 3D',pendiente);
+INSERT INTO unidades VALUES('clavemateria-4',4,'Relleno, Iluminacion y Sombreado',pendeinte);
