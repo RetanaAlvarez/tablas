ba743795ebb8068942e8afde57090d175d44af3f
Alex <73196720+Alx-Ang@users.noreply.github.com>
2020-11-24 19:47:33 -0600

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -15,6 +15,18 @@ CONSTRAINT materia_unidades FOREIGN KEY(clave_materia) REFERENCES materias(clave
 ON DELETE CASCADE
 );
 --agregar sus incersiones de codigo de la tabla alumno
+
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+INSERT INTO alumnos VALUES ('2018150480052','----');
+
 INSERT INTO alumnos VALUES('2018150480195',pendiente);
 INSERT INTO alumnos VALUES('2018150480034',pendiente);
 INSERT INTO alumnos VALUES('2018150480572',pendiente);
@@ -26,6 +38,18 @@ INSERT INTO alumnos VALUES('2018150480073',pendiente);
 INSERT INTO alumnos VALUES('2018150480724',pendiente);
 INSERT INTO alumnos VALUES('2018150480936',pendiente);
 --agregar sus increciones de codigo de la tabla unidades
+
+INSERT INTO unidades VALUES ('clavemateria-1',1,'Arquitecturas de cómputo',----);
+INSERT INTO unidades VALUES ('clavemateria-2',2,'Estructura y funcionamiento de la CPU',----);
+INSERT INTO unidades VALUES ('clavemateria-3',3,'Componentes para ensamble de equipo de computo',----);
+INSERT INTO unidades VALUES ('clavemateria-4',4,'Procesamiento paralelo',----);
+INSERT INTO unidades VALUES ('clavemateria-1',1,'Sistemas operativos',----);
+INSERT INTO unidades VALUES ('clavemateria-2',2,'Administracion de procesos y hyperprocesador',----);
+INSERT INTO unidades VALUES ('clavemateria-3',3,'Administracion de memoria',----);
+INSERT INTO unidades VALUES ('clavemateria-4',4,'Administracion de Entrada-Salida',----);
+INSERT INTO unidades VALUES ('clavemateria-5',5,'Sistemas de archivos',----);
+INSERT INTO unidades VALUES ('clavemateria-6',6,'Proteccion y seguridad',----);
+
 INSERT INTO unidades VALUES('clavemateria-1',1,'Fundamentos de Ingeneria en Software',pendiente);
 INSERT INTO unidades VALUES('clavemateria-5',5,'Introduccion a la Animacion Por Computadora',pendiente);
 INSERT INTO unidades VALUES('clavemateria-2',2,'El modelo de negocio',pendiente);
